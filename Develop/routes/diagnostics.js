72425dc3d071e48ca7bd30b72f902c34824f4661
Shmuel Hoffman <snyh212@gmail.com>
2022-07-22 11:52:04 -0700

    added git ignore installed express npm and added diagnostics post

--- Develop/routes/diagnostics.js
+++ Develop/routes/diagnostics.js
@@ -5,11 +5,33 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostic.json').then((data) => res.json(JSON.parse(data)))
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  const { time, errors } = req.body;
+
+  if (time && errors) {
+    // Variable for the object we will save
+    const newDiagnostics = {
+      time,
+      errrors,
+      error_id: uuidv4(),
+    };
+
+    readAndAppend(newDiagnostics, './db/diagnostics.json');
+
+    const response = {
+      status: 'success',
+      body: newDiagnostics,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting diagnostics');
+  }
 });
 
 module.exports = diagnostics;
